be06575d559c7ec3d5936553c8ebbc2c29786401
karengomezs <karengomezs0601@gmail.com>
2023-05-04 11:16:39 -0500

    details save

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -21,26 +21,39 @@ type Props = {
   response: Result | null;
 };
 
+function extractedNumber(text: string | undefined): string | undefined {
+  const textArr = text?.split("/");
+  const number = textArr?.[textArr.length - 1];
+  return number;
+}
+
 export default function CharacterDetails(props: Props) {
   const episodes = props.response?.episode.map((episode, i) => {
-    const textArr = episode.split("/");
-    const number = textArr[textArr.length - 1];
+    // const textArr = episode.split("/");
+    // const number = textArr[textArr.length - 1];
 
     return (
-      <Link key={i} href={`/episode?number=${number}`}>
-        <li>Episode {number}</li>
+      <Link key={i} href={`/episode?number=${extractedNumber(episode)}`}>
+        <li>Episode {extractedNumber(episode)}</li>
       </Link>
     );
   });
+
   return (
     <>
       <h1 className="text-3xl text-center">DETAILS</h1>
       <main className="flex flex-col min-h-screen py-10 px-4 max-w-6xl mx-auto">
         <img className="w-1/3" src={props.response?.image} alt="" />
-        <h3>{props.response?.gender}</h3>
-        <h3>{props.response?.name}</h3>
-        <h3>{props.response?.location.name}</h3>
-        <h3>{props.response?.species}</h3>
+        <h3>Gender: {props.response?.gender}</h3>
+        <h3>Name: {props.response?.name}</h3>
+        <h3>Especie: {props.response?.species}</h3>
+        <Link
+          href={`locationDetails?number=${extractedNumber(
+            props.response?.location.url
+          )}`}
+        >
+          <h3>Location: {props.response?.location.name}</h3>
+        </Link>
         <h3>EPISODES</h3>
         <ul>{episodes}</ul>
       </main>
