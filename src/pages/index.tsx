ed215a91ecb05cb3613d05ac1b55f1234e00c1d1
karengomezs <karengomezs0601@gmail.com>
2023-05-10 21:14:20 -0500

    deleted unused components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { RootObject } from "@/api/callsApi";
 import { Button } from "@/components/ui/button";
 import HoverEffect from "@/components/Hoverffect";
 import H1 from "@/components/H1";
-import Navbar from "@/components/Navbar";
 
 export const getServerSideProps = async () => {
   type UrlsT = string;
